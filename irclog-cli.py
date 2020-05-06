6d3b3ebda17e0db5e95d7fe720ad439a2b00b90a
Дамјан Георгиевски <gdamjan@gmail.com>
2020-05-07 01:32:03 +0200

    apply a linter

--- irclog-cli.py
+++ irclog-cli.py
@@ -8,40 +8,57 @@ import json
 from datetime import datetime
 
 global VERBOSE
-IRCLOG_URL = 'https://irc.softver.org.mk/'
+IRCLOG_URL = "https://irc.softver.org.mk/"
+
 
 def print_message(doc):
     if VERBOSE:
         pprint.pprint(doc)
         return
 
-    tm = datetime.fromtimestamp(doc['timestamp'])
-    tm = tm.strftime('%H:%M:%S')
-    print("%s %s: %s" % (tm, doc['sender'], doc['message']))
+    tm = datetime.fromtimestamp(doc["timestamp"])
+    tm = tm.strftime("%H:%M:%S")
+    print("%s %s: %s" % (tm, doc["sender"], doc["message"]))
+
 
 def get_backlog(channel, limit=100):
-    url = urljoin(IRCLOG_URL, '/ddoc/_view/channel')
-    startkey = [channel,{}]
-    endkey = [channel,0]
+    url = urljoin(IRCLOG_URL, "/ddoc/_view/channel")
+    startkey = [channel, {}]
+    endkey = [channel, 0]
 
-    query = dict(update_seq='true', reduce='false', descending='true',
-            limit=limit, include_docs='true', startkey=startkey, endkey=endkey)
+    query = dict(
+        update_seq="true",
+        reduce="false",
+        descending="true",
+        limit=limit,
+        include_docs="true",
+        startkey=startkey,
+        endkey=endkey,
+    )
     req = requests.post(url, json=query)
     if not req.ok:
         req.raise_for_status()
 
     backlog = req.json()
-    update_seq = backlog['update_seq']
+    update_seq = backlog["update_seq"]
+
     def _gen():
-        for row in sorted(backlog['rows'], key=lambda r: r['doc']['timestamp']):
-            yield row['doc']
+        for row in sorted(backlog["rows"], key=lambda r: r["doc"]["timestamp"]):
+            yield row["doc"]
+
     return update_seq, _gen()
 
+
 def get_changes(channel, since):
-    url = urljoin(IRCLOG_URL, '/api/_changes')
-    query = dict(feed='continuous', filter='_selector',
-            heartbeat=30000, include_docs='true', since=since)
-    data = { 'selector': {'channel':channel}}
+    url = urljoin(IRCLOG_URL, "/api/_changes")
+    query = dict(
+        feed="continuous",
+        filter="_selector",
+        heartbeat=30000,
+        include_docs="true",
+        since=since,
+    )
+    data = {"selector": {"channel": channel}}
     req = requests.post(url, params=query, json=data, stream=True, timeout=60)
     if not req.ok:
         req.raise_for_status()
@@ -49,41 +66,46 @@ def get_changes(channel, since):
     for row in req.iter_lines(chunk_size=None, decode_unicode=True):
         if row.strip():
             change = json.loads(row)
-            doc = change['doc']
-            yield change['seq'], doc
+            doc = change["doc"]
+            yield change["seq"], doc
+
 
 def list_channels(_args):
-    url = urljoin(IRCLOG_URL, '/ddoc/_view/channel')
-    query = {'group_level': 1}
+    url = urljoin(IRCLOG_URL, "/ddoc/_view/channel")
+    query = {"group_level": 1}
     req = requests.post(url, json=query)
     if not req.ok:
         req.raise_for_status()
 
-    for ch in req.json()['rows']:
-        print(ch['key'][0], ch['value'])
+    for ch in req.json()["rows"]:
+        print(ch["key"][0], ch["value"])
+
 
 def search(args):
-    needle = ' '.join(args.needle)
-    url = urljoin(IRCLOG_URL, '/api/_find?include_docs=true')
-    q = { "selector": {
-                "channel": args.channel,
-                "message": {"$regex": needle },
-                # "timestamp": { "$gt" или "$lt": 1537798438 }
-            },
-            "fields": ["_id", "timestamp", "sender", "message", "channel"]
-        }
+    needle = " ".join(args.needle)
+    url = urljoin(IRCLOG_URL, "/api/_find?include_docs=true")
+    q = {
+        "selector": {
+            "channel": args.channel,
+            "message": {"$regex": needle},
+            # "timestamp": { "$gt" или "$lt": 1537798438 }
+        },
+        "fields": ["_id", "timestamp", "sender", "message", "channel"],
+    }
     req = requests.post(url, json=q)
     if not req.ok:
         req.raise_for_status()
 
-    for doc in req.json()['docs']:
+    for doc in req.json()["docs"]:
         print_message(doc)
+
 
 def follow(args):
     update_seq, msgs = get_backlog(args.channel, limit=args.limit)
     for msg in msgs:
         print_message(msg)
     loop(args.channel, update_seq)
+
 
 def loop(channel, update_seq):
     done = False
@@ -99,30 +121,31 @@ def loop(channel, update_seq):
             pass
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--verbose', '-v', action='count')
-    subparsers = parser.add_subparsers(title='subcommands')
+    parser.add_argument("--verbose", "-v", action="count")
+    subparsers = parser.add_subparsers(title="subcommands")
 
-    s = subparsers.add_parser('search', help='search <channel> <text…>')
-    s.add_argument('channel')
-    s.add_argument('needle', metavar='text', nargs='+', help='text to search for')
+    s = subparsers.add_parser("search", help="search <channel> <text…>")
+    s.add_argument("channel")
+    s.add_argument("needle", metavar="text", nargs="+", help="text to search for")
     s.set_defaults(func=search)
 
-    f = subparsers.add_parser('follow', help='follow <channel> [backlog limit, default 100]')
-    f.add_argument('channel', help='channel to look at')
-    f.add_argument('limit', default=100, nargs='?', type=int, help='limit backlog')
+    f = subparsers.add_parser(
+        "follow", help="follow <channel> [backlog limit, default 100]"
+    )
+    f.add_argument("channel", help="channel to look at")
+    f.add_argument("limit", default=100, nargs="?", type=int, help="limit backlog")
     f.set_defaults(func=follow)
 
-    l = subparsers.add_parser('list', help='list all channels')
+    l = subparsers.add_parser("list", help="list all channels")
     l.set_defaults(func=list_channels)
 
     args = parser.parse_args()
     VERBOSE = args.verbose
-    if 'func' in args:
+    if "func" in args:
         args.func(args)
     else:
         parser.print_help()
